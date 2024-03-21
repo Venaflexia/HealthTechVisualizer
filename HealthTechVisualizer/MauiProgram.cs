7f23f3d99d5c0b8f1a5705352892b635445b4613
Babürşah Baz <bazbabursah1221@gmail.com>
2024-03-21 21:24:17 +0300

    The MVVM structure has been set up, and some necessary libraries have been downloaded for the initial setup.

--- HealthTechVisualizer/MauiProgram.cs
+++ HealthTechVisualizer/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace HealthTechVisualizer
 {
@@ -9,6 +10,8 @@ namespace HealthTechVisualizer
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .UseMauiCommunityToolkitMediaElement()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
