e085506c9641d5241848510a0f804cfa50f4e4c6
Babürşah Baz <bazbabursah1221@gmail.com>
2024-03-25 00:06:38 +0300

    report menu was created and binding with title

--- HealthTechVisualizer/Mvvm/Views/VisualizerPage.xaml.cs
+++ HealthTechVisualizer/Mvvm/Views/VisualizerPage.xaml.cs
@@ -2,10 +2,54 @@ namespace HealthTechVisualizer.Mvvm.Views;
 
 public partial class VisualizerPage : ContentPage
 {
+	List<string> MenuList;
+	int MenuIndex;
 	public VisualizerPage()
 	{
 		InitializeComponent();
+		MenuList = new List<string>();
+		MenuIndex = 1;
 	}
 
-    
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+        MenuList.Add("Visualizer");
+        MenuList.Add("Marker");     
+        MenuList.Add("Rapor");
+        TitleIndicator.Count = MenuList.Count;
+        TitleIndicator.Position = 1;
+    }
+    private void HeaderChangerTap(object sender, TappedEventArgs e)
+    {
+		string result =  e.Parameter.ToString();
+		
+		switch (result)
+		{
+			case "Next":
+				if(MenuIndex<=1)
+				{ MenuIndex += 1; }
+
+                break;
+            case "Back":
+				if(MenuIndex>=1)
+                { MenuIndex -= 1; }
+                
+                break;
+				
+        }
+        MenuTitle.Text = MenuList[MenuIndex];
+        TitleIndicator.Position = MenuIndex;
+        switch (MenuIndex)
+        {
+            case 1:
+                MarkerLayout.IsVisible = true;
+                RaporLayout.IsVisible = false;
+                break;
+            case 2:
+                MarkerLayout.IsVisible = false;
+                RaporLayout.IsVisible = true;
+                break;
+        }
+    }
 }
